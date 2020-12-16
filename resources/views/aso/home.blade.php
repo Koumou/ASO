18d8a06135cb64639f44db2b110b996070f98b39
kessel okinga <kesselokinga@gmail.com>
2020-12-16 19:25:08 +0200

    initial

---
+++ resources/views/aso/home.blade.php
@@ -0,0 +1,120 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <br />
+    <center>
+    <div>
+        <i class="fas fa-globe-africa col d-flex justify-content-center pb-3" style="font-size:38px;color:#236E96;"></i>
+    </div>
+    <div class="col justify-content-center">
+        <h3 class="text-center"><b>I just want to share an exploration of the landing page online Accountancy Services</b></h3>
+    <!-- </div>
+    <div class="col d-flex justify-content-center mx-4 px-4 text-center"> -->
+        <p class="text-center">
+            I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style. I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+        </p>
+        
+    </div></center>
+
+    <br />
+    <ul class="nav justify-content-center pb-4">
+        <li class="nav-item">
+            <a class="nav-link" style="font-size:large; color:#23375b;"> <i class="fas fa-file-invoice	 pr-1" style="font-size:22px;color:#8134AF; vertical-align:baseline;"></i><b><u>Proposals</u></b></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" style="font-size:large; color:#23375b;"> <i class="fas fa-balance-scale-left pr-1" style="font-size:22px;color:#F3872F; vertical-align:baseline;"></i><b><u>Balance scale</u></b></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" style="font-size:large; color:#23375b;"> <i class="fas fa fa-calculator pr-1" style="font-size:22px;color:#0091A0; vertical-align:baseline;"></i><b><u>Accounting</u></b></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" style="font-size:large; color:#23375b;"> <i class="fas fa-pencil-alt pr-1" style="font-size:22px;color:#FF7682; vertical-align:baseline;"></i><b><u>Adjust</u></b></a>
+        </li>
+    </ul>
+    <br />
+    <br />
+    <div class="row pb-3" ">
+        <div class=" col-lg">
+        <img src="https://images.assetsdelivery.com/compings_v2/thevisualsyouneed/thevisualsyouneed1810/thevisualsyouneed181000293.jpg" style="width: 100%; max-width:100vw;max-height:unset;" alt="...">
+    </div>
+    <div class="col-lg  align-self-center">
+    <h3 class="pt-3">Input</h3>
+        <div class="linecolorh1">
+            <hr class="linehr">
+        </div>
+        I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+        I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+        I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+        I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+    </div>
+
+
+</div>
+<br />
+
+
+<div class="row pt-4" style="background-color: #002760; color:whitesmoke; border-radius: 15px 15px 15px 15px;">
+    <div class="col-lg-6">
+        <div class="pl-3">
+            <span class="badge badge-primary" style="background-color: #8134AF;">Proposals</span>
+
+        </div>
+        <div class="card border-0 bg-transparent">
+            <div class="card-body">
+                I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+                I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style. </div>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="pl-3">
+            <span class="badge badge-primary" style="background-color: #F3872F;">Balance scale</span>
+        </div>
+        <div class="card border-0 bg-transparent">
+            <div class="card-body">
+                I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+                I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style. </div>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="pl-3">
+            <span class="badge badge-primary" style="background-color: #0091A0;">Accounting</span>
+        </div>
+        <div class="card border-0 bg-transparent">
+            <div class="card-body">
+                I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+                I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style. </div>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="pl-3">
+            <span class="badge badge-primary" style="background-color: #FF7682;">Adjust</span>
+        </div>
+        <div class="card border-0 bg-transparent">
+            <div class="card-body">
+                I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+                I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+                I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style. </div>
+        </div>
+    </div>
+</div>
+
+<br />
+<div class="row pt-3 flex-row-reverse">
+    <div class=" col-lg ">
+        <img src="https://thumbs.dreamstime.com/b/%E5%B9%B4%E8%BD%BB%E6%84%89%E5%BF%AB%E5%92%8C%E5%8F%AF%E7%88%B1%E7%9A%84%E8%A1%8C%E5%AE%B6%E9%BB%91%E8%89%B2%E7%BE%8E%E5%9B%BD%E9%BB%91%E4%BA%BA%E7%9A%84%E5%A6%87%E5%A5%B3%E9%A5%AE%E7%94%A8%E7%9A%84%E8%8C%B6%E6%88%96%E5%92%96%E5%95%A1%E5%9C%A8%E5%AE%B6%E5%8A%9E%E5%85%AC%E5%AE%A4%E5%B7%A5%E4%BD%9C%E5%BF%AB%E4%B9%90%E4%B8%8E%E8%86%9D%E4%B8%8A%E5%9E%8B%E8%AE%A1%E7%AE%97%E6%9C%BAco-118136289.jpg" style="width: 100%; max-width:100vw;max-height:unset;" alt="...">
+    </div>
+    <div class="col-lg  align-self-center ">
+        <!-- <h1 class="textformat gradient-text">Card title</h1> -->
+        <h3 class="pt-3" >Output</h3>
+        <div class="linecolorh2 ">
+            <hr class="linehr">
+        </div>
+
+        I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+        I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+    </div>
+</div>
+
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,379 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<!-- <style>
+  
+
+    body {
+        font-family: 'Raleway', sans-serif;
+        font-size: 14px;
+        color: #9a9a9a;
+    }
+
+    body .testimonial {
+        padding: 100px 0;
+    }
+
+    body .testimonial .row .tabs {
+        all: unset;
+        margin-right: 50px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    body .testimonial .row .tabs li {
+        all: unset;
+        display: block;
+        position: relative;
+    }
+
+    body .testimonial .row .tabs li:nth-child(1) {
+        align-self: flex-end;
+    }
+
+    body .testimonial .row .tabs li:nth-child(1)::before {
+        left: 64%;
+        bottom: -50px;
+    }
+
+    body .testimonial .row .tabs li:nth-child(1)::after {
+        right: 97%;
+        bottom: -81px;
+    }
+
+    body .testimonial .row .tabs li:nth-child(1) figure img {
+        margin-left: auto;
+    }
+
+    body .testimonial .row .tabs li:nth-child(2) {
+        align-self: flex-start;
+    }
+
+    body .testimonial .row .tabs li:nth-child(2)::before {
+        right: -65px;
+        top: 50%;
+    }
+
+    body .testimonial .row .tabs li:nth-child(2)::after {
+        bottom: 101px;
+        border-radius: 50%;
+        right: -120px;
+    }
+
+    body .testimonial .row .tabs li:nth-child(2) figure img {
+        margin-right: auto;
+        max-width: 300px;
+        width: 100%;
+        margin-top: -50px;
+    }
+
+    body .testimonial .row .tabs li:nth-child(3) {
+        align-self: flex-end;
+    }
+
+    body .testimonial .row .tabs li:nth-child(3)::before {
+        right: -10px;
+        top: -66%;
+    }
+
+    body .testimonial .row .tabs li:nth-child(3)::after {
+        top: -130px;
+        border-radius: 50%;
+        right: -46px;
+    }
+
+    body .testimonial .row .tabs li:nth-child(3) figure img {
+        margin-left: auto;
+        margin-top: -50px;
+    }
+
+    body .testimonial .row .tabs li:nth-child(3):focus {
+        border: 10px solid red;
+    }
+
+    body .testimonial .row .tabs li figure {
+        position: relative;
+    }
+
+    body .testimonial .row .tabs li figure img {
+        display: block;
+    }
+
+    body .testimonial .row .carousel>h3 {
+        font-size: 20px;
+        line-height: 1.45;
+        color: rgba(0, 0, 0, 0.5);
+        font-weight: 600;
+        margin-bottom: 0;
+    }
+
+    body .testimonial .row .carousel h1 {
+        font-size: 40px;
+        line-height: 1.225;
+        margin-top: 23px;
+        font-weight: 700;
+        margin-bottom: 0;
+    }
+
+    body .testimonial .row .carousel .carousel-indicators {
+        all: unset;
+        padding-top: 43px;
+        display: flex;
+        list-style: none;
+    }
+
+    body .testimonial .row .carousel .carousel-indicators li {
+        background: #000;
+        background-clip: padding-box;
+        height: 2px;
+    }
+
+    body .testimonial .row .carousel .carousel-inner .carousel-item .quote-wrapper {
+        margin-top: 42px;
+    }
+
+    body .testimonial .row .carousel .carousel-inner .carousel-item .quote-wrapper p {
+        font-size: 18px;
+        line-height: 1.72222;
+        font-weight: 500;
+        color: rgba(0, 0, 0, 0.7);
+    }
+
+    body .testimonial .row .carousel .carousel-inner .carousel-item .quote-wrapper h3 {
+        color: #000;
+        font-weight: 700;
+        margin-top: 37px;
+        font-size: 20px;
+        line-height: 1.45;
+        text-transform: uppercase;
+    }
+</style> -->
+<style>
+    * {
+        transition: all 0.2s ease-in-out;
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+
+    .linecolorh2 {
+        background-image: linear-gradient(to right, whitesmoke, #008000);
+        width: 40%;
+        text-align: left;
+        margin-left: 0;
+    }
+
+    .linecolorh1 {
+        background-image: linear-gradient(to right, red 80%, whitesmoke);
+        width: 40%;
+        text-align: left;
+        margin-left: 0;
+    }
+
+    .linehr {
+        border-width: 4px;
+
+    }
+
+    .pointerscrollup {
+        cursor: pointer;
+    }
+
+    .navbar-nav {
+        font-size: 1rem;
+        font-family: 'Source Sans Pro', sans-serif;
+    }
+</style>
+
+<body>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <div class="container">
+                <a class="navbar-brand">ASO</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#!">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#!">Testimonials</a>
+                        </li>
+                        <li class="nav-item mr-4 ">
+                            <a class="nav-link" href="#!">About Us</a>
+                        </li>
+                    </ul>
+                    <span class="navbar-text">
+                        <a class="btn btn-info text-light" href="#" role="button">Contact Us</a>
+                    </span>
+                </div>
+            </div>
+        </nav>
+
+        <!-- <section class="testimonial">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 d-none d-lg-block">
+                        <ol class="carousel-indicators tabs">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0">
+                                <figure>
+                                    <img src="https://livedemo00.template-help.com/wt_62267_v8/prod-20823-one-service/images/testimonials-01-179x179.png" class="img-fluid" alt="">
+                                </figure>
+                            </li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1">
+                                <figure>
+                                    <img src="https://livedemo00.template-help.com/wt_62267_v8/prod-20823-one-service/images/testimonials-02-306x306.png" class="img-fluid" alt="">
+                                </figure>
+                            </li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2">
+                                <figure>
+                                    <img src="https://livedemo00.template-help.com/wt_62267_v8/prod-20823-one-service/images/testimonials-03-179x179.png" class="img-fluid" alt="">
+                                </figure>
+                            </li>
+                        </ol>
+                    </div>
+                    <div class="col-lg-6 d-flex justify-content-center align-items-center">
+                        <div id="carouselExampleIndicators" data-interval="false" class="carousel slide" data-ride="carousel">
+                            <h3>WHAT OUR CLIENTS SAY</h3>
+                            <h1>TESTIMONIALS</h1>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <div class="quote-wrapper">
+                                        <p>I have tried a lot of food delivery services but Plate is something out of this world! Their food is really healthy and it tastes great, which is why I recommend this company to all my friends!</p>
+                                        <h3>peter lee</h3>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="quote-wrapper">
+                                        <p>I have tried a lot of food delivery services but Plate is something out of this world! Their food is really healthy and it tastes great, which is why I recommend this company to all my friends!</p>
+                                        <h3>peter lee</h3>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="quote-wrapper">
+                                        <p>I have tried a lot of food delivery services but Plate is something out of this world! Their food is really healthy and it tastes great, which is why I recommend this company to all my friends!</p>
+                                        <h3>peter lee</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- <ol class="carousel-indicators indicators">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        </ol> -->
+        <!-- </div>
+    </div>
+    </div>
+    </div> -->
+        <!-- </section> -->
+        <br />
+        <div class="container">
+
+            <div class="row pt-4">
+                <!-- <div class="col-lg-6  align-self-center ">
+         
+
+            I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+            I just want to share an exploration of the landing page online Accountancy Services. my idea is to make a more simple and clean style.
+        </div> -->
+
+                <div class="col-lg">
+                    <img src="https://www.bizlibrary.com/wp-content/uploads/2020/06/22145936/diverse-team-collaborating-at-work.jpg" style="width: 100%; max-width:100vw;max-height:unset;" alt="">
+                </div>
+            </div>
+        </div>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    <footer class="bg-light text-center text-lg-start">
+        <div class="container p-4">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 mb-2">
+                    <h5 class="text-uppercase"> <i class="fas fa-globe-africa col d-flex justify-content-center pb-3" style="font-size:38px;color:#236E96;"></i>
+                    </h5>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+                        molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+                        voluptatem veniam, est atque cumque eum delectus sint!
+                    </p>
+                </div>
+
+                <div class="col-lg-3 col-md-6 mt-4 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href="#!" class="text-dark">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-dark">Contact Us</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-dark">T&Cs</a>
+                        </li>
+
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6 mt-4 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Follow us</h5>
+                    <ul class="nav justify-content-center list-unstyled mb-0">
+                        <li>
+                            <a class="nav-link" href="#"> <i class="fa fa-facebook-f" style="font-size:20px;color:#236E96;"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#"> <i class="fa fa-google" style="font-size:20px;color:#236E96;"></i>
+                            </a>
+                        </li>
+
+                    </ul>
+
+                </div>
+            </div>
+            <i class='fas fa-arrow-alt-circle-up float-right pointerscrollup' onclick="scrollToTop()" style='font-size:38px;color:#002760'></i>
+        </div>
+        <!-- <footer class="text-light text-center text-lg-start" style="background-color: #236E96;"> -->
+
+        <!-- Copyright -->
+        <div class="text-light text-center p-3 " style="background-color: #236E96;">
+            © 2020 Copyright:
+            <a class="text-light" href="#">aso.cg</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <script>
+        function scrollToTop() {
+            $(window).scrollTop(0);
+        }
+    </script>
+</body>
+
+</html>
